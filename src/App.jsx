e536e58a73cd45afc1d3308da16cc878b47da956
Isis Zapata Florentino <92564676+isinicolle@users.noreply.github.com>
2023-01-18 04:46:50 +0000

    Personalizacion de nuestro portafolio

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Isis Zapata",
+  title: "Full Stack Developer & QA",
+  email: "isinicollee@gmail.com",
+  gitHub: "isinicolle",
+  instagram: "isinicolle",
+  linkedIn: "isinicolle",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "isinicolle",
+  youTube: "isinicolle",
 };
 
 const primaryColor = "#4E567E";
